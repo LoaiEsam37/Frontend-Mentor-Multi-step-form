37d859a0b080f9cabc2245dcae9953e60b8997d5
LoaiEsam37 <loaie1199@gmail.com>
2023-03-29 17:55:32 +0200

    commit

---
+++ pages/index.jsx
@@ -0,0 +1,46 @@
+import { Ubuntu } from "next/font/google";
+import Head from "next/head";
+import "@/styles/Home.module.css";
+
+const ubuntu = Ubuntu({
+  subsets: [
+    "cyrillic",
+    "cyrillic-ext",
+    "greek",
+    "greek-ext",
+    "latin",
+    "latin-ext",
+  ],
+  weight: ["400", "500", "700"],
+});
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Frontend Mentor | Multi-step form</title>
+        <meta
+          name="description"
+          content="Frontend Mentor Multi-step form. made by LoaiEsam37"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+      </Head>
+      <main>
+        <div className="card">
+          <div className="card__sidebar">
+            <div className="img__wrapper">
+              <img src="/bg-sidebar-desktop.svg" />
+            </div>
+          </div>
+          <div className="card__content"></div>
+        </div>
+      </main>
+    </>
+  );
+}
