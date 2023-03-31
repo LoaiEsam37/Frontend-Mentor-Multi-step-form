828f0f863cd5145eefa3ae386343e828c258dbd9
LoaiEsam37 <loaie1199@gmail.com>
2023-03-31 09:48:39 +0200

    commit

---
+++ slices/dataObjectSlice.ts
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { RootState } from "../store";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+// Define a type for the slice state
+interface DataState {
+  value: number;
+}
+
+// Define the initial state using that type
+const initialState: DataState = {
+  value: 0,
+};
+
+export const dataObjectSlice = createSlice({
+  name: "dataObject",
+  initialState: {
+    value: {},
+  },
+  reducers: {
+    assignObject: (state, action: PayloadAction<object>) => {
+      state.value = Object.assign(state.value, action.payload);
+    },
+  },
+});
+
+export const { assignObject } = dataObjectSlice.actions;
+
+export const selectStepCount = (state: RootState) => state.dataObject.value;
+
+export default dataObjectSlice.reducer;
--- store.ts
+++ store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import stepCounterSlice from "./slices/stepCounterSlice";
+import dataObjectSlice from "./slices/dataObjectSlice";
 
 export const store = configureStore({
   reducer: {
     stepCounter: stepCounterSlice,
+    dataObject: dataObjectSlice,
   },
 });
 
